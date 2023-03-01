digital-blueprint/relay-mono-connector-campusonline-bundle

commit de5e13312fe0ec23c67d607a4642990c74c3bb31

Notify co on complete status only

src/Service/TuitionFeeService.php
@@ -123,16 +123,18 @@ class TuitionFeeService extends AbstractPaymentTypesService implements BackendSe
         $notified = false;
 
         if (!$payment->getNotifiedAt()) {
-            $type = $payment->getType();
-            $api = $this->getApiByType($type);
-            $obfuscatedId = $payment->getLocalIdentifier();
-            $amount = (float) $payment->getAmount();
-            $semesterKey = Tools::convertSemesterToSemesterKey($payment->getData());
-            try {
-                $api->registerPaymentForSemester($obfuscatedId, $amount, $semesterKey);
-            } catch (ApiException $e) {
-                $this->logger->error('Communication error with backend!', ['exception' => $e]);
-                throw new ApiError(Response::HTTP_INTERNAL_SERVER_ERROR, 'Communication error with backend!');
+            if ($payment->getPaymentStatus() === Payment::PAYMENT_STATUS_COMPLETED) {
+                $type = $payment->getType();
+                $api = $this->getApiByType($type);
+                $obfuscatedId = $payment->getLocalIdentifier();
+                $amount = (float) $payment->getAmount();
+                $semesterKey = Tools::convertSemesterToSemesterKey($payment->getData());
+                try {
+                    $api->registerPaymentForSemester($obfuscatedId, $amount, $semesterKey);
+                } catch (ApiException $e) {
+                    $this->logger->error('Communication error with backend!', ['exception' => $e]);
+                    throw new ApiError(Response::HTTP_INTERNAL_SERVER_ERROR, 'Communication error with backend!');
+                }
             }
             $notified = true;
         }

commit bf90c4211f30bf362ae97fa24139b03402888de4

Only return notified if we actually did it
fallout from de5e13312fe0ec23c67d607

src/Service/TuitionFeeService.php
@@ -157,8 +157,8 @@ class TuitionFeeService extends AbstractPaymentTypesService implements BackendSe
                     $this->logger->error('Communication error with backend!', ['exception' => $e]);
                     throw new ApiError(Response::HTTP_INTERNAL_SERVER_ERROR, 'Communication error with backend!');
                 }
+                $notified = true;
             }
-            $notified = true;
         }
 
         return $notified;
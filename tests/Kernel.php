1bbe1d83edbeab523644b09e411b196798a8485c
Christoph Reiter <reiter.christoph@gmail.com>
2023-05-15 12:48:06 +0200

    Update to api-platform 2.7

--- tests/Kernel.php
+++ tests/Kernel.php
@@ -92,5 +92,9 @@ class Kernel extends BaseKernel
                 ],
             ],
         ]);
+
+        $container->extension('api_platform', [
+            'metadata_backward_compatibility_layer' => false,
+        ]);
     }
 }
